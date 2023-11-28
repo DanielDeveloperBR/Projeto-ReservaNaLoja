f0abf19397a28d0253d2ca5f7ad3ea6274f5b0b7
Daniel <danieldetrabalho@hotmail.com>
2023-11-28 20:17:13 -0300

    Melhorei no loading e no modal e no estilo

--- js/loading.js
+++ js/loading.js
@@ -1,13 +1,17 @@
-// Adicione isso ao seu script JS existente
-document.addEventListener('DOMContentLoaded', () => {
-    const loadingSpinner = document.getElementById('loading-spinner');
- 
-    // Mostra a bolinha giratória
-    loadingSpinner.style.display = 'block';
- 
-    window.addEventListener('load', () => {
-       // Esconde a bolinha giratória quando a página está totalmente carregada
-       loadingSpinner.style.display = 'none';
-    });
- });
- 
+
+let modal = document.querySelector('#modal')
+// Função para mostrar o indicador de carregamento
+function mostrarLoanding() {
+   const loadingSpinner = document.getElementById('loading-spinner');
+   modal.classList.add("modal")
+   loadingSpinner.style.display = 'block';
+}
+
+// Função para esconder o indicador de carregamento
+function esconderLoading() {
+   const loadingSpinner = document.getElementById('loading-spinner');
+   modal.classList.remove("modal")
+   loadingSpinner.style.display = 'none';
+   document.addEventListener('DOMContentLoaded', () => {
+   })
+}
